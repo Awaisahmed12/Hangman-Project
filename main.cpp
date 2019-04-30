0715517ccaf794a8dfbbe5c7092c793a7d4d0897
Jeremiah Lewis <28968799+jeremiahlewis77@users.noreply.github.com>
2019-04-30 17:01:47 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,11 +7,47 @@
 using namespace std;
 
 int main(){
-	
-	
-	
-	
-	
-	
-return 0;	
+    // Declarations
+    gameMan game;
+    char letter;
+    int diff, option;
+    // -----------
+
+    game.printMenu(0);
+
+    cin >> option;
+
+    if (option == 1){
+        game.printMenu(1);
+    }
+    else if (option == 2){
+
+    }
+    else if (option == 3){
+        return 0;
+    }
+
+    cin >> diff;
+
+    game.getWords(diff);
+    wordToSolve word(game.pickWord());
+    game.gWord = word;
+
+
+    while (game.gWord.getAttempts() < 6) {
+        game.printMenu(2);
+
+        cin >> letter;
+        if (letter == '$') {
+            game.saveFile();
+            return 0;
+        }
+
+        game.gWord.solve(letter);
+    }
+
+    cout << "The word was: " << game.gWord.getWord() << endl;
+
+    return 0;
 }
+
