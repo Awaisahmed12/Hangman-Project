3c28591447f02623de56f4e5677e6e40371527bf
Jeremiah Lewis <28968799+jeremiahlewis77@users.noreply.github.com>
2019-04-30 17:02:04 -0500

    Update wordToSolve.h

--- wordToSolve.h
+++ wordToSolve.h
@@ -2,19 +2,37 @@
 #define WORDTOSOLVE_H
 #include <string>
 #include <iostream>
+#include <vector>
 using namespace std;
 
-class wordToSolve{
-	
+class wordToSolve {
+
 	public:
 		wordToSolve(){
 			word = "";
-		}	
-		
-	
+		}
+
+        wordToSolve(string sWord);
+        void solve(char letter);
+        int getAttempts(){return attempts;}
+        void printUnderscores();
+        void printGuesses();
+        string getWord() {return word;}
+        vector<char> getUnderscore() {return underscore;}
+        vector<char> getGuesses() {return guesses;}
+        bool Solved();
+        void printHangman(int o);
+        wordToSolve &operator=(wordToSolve &otherWord);
+
+
 	private:
 		string word;
-		
-	
-	
+		vector<char> underscore;
+		vector<char> guesses;
+		int attempts = 0;
+
+
+
 };
+
+#endif
