516fb2f28b4211f42c77499a6b1641258c3a9ebd
Jeremiah Lewis <28968799+jeremiahlewis77@users.noreply.github.com>
2019-05-02 17:22:39 -0500

    Update wordToSolve.cpp

--- wordToSolve.cpp
+++ wordToSolve.cpp
@@ -7,6 +7,9 @@ wordToSolve::wordToSolve(string sWord) {
     while (underscore.size() != sWord.size()) {
         underscore.push_back('_');
     }
+
+    guesses.clear();
+    wordGuesses.clear();
 }
 
 wordToSolve::wordToSolve(string sWord, int sAttempts, vector<char> sUnderscore, vector<char> sGuesses) {
@@ -24,14 +27,40 @@ wordToSolve::wordToSolve(string sWord, int sAttempts, vector<char> sUnderscore, 
 
 
 }
-bool wordToSolve::solve(char letter) {
+bool wordToSolve::solve(string letter) {
     bool isFound = false;
     bool underscoreFound = false;
     bool guessFound = false;
+    char letterChar;
+    bool isWord = false;
+
+    cout.width(28);
+    cout.fill('*');
+    cout << "" << endl << endl << endl;
+
+    if (letter.length() != 1) {
+        isWord = true;
+    } else if (letter.length() == 1) {
+        letterChar = letter[0];
+    }
+
+    if (isWord) {
+        if (letter == word){
+            for (int i = 0; i < word.length(); ++i) {
+                underscore[i] = word[i];
+            }
+            return true;
+        }
+        else {
+            attempts++;
+            wordGuesses.push_back(letter);
+            }
+        return false;
+    }
 
 
     for (int i = 0; i < word.size(); ++i) {
-        if (tolower(letter) == tolower(word[i])) {
+        if (tolower(letterChar) == tolower(word[i])) {
             isFound = true;
             underscore[i] = tolower(word[i]);
         }
@@ -39,7 +68,7 @@ bool wordToSolve::solve(char letter) {
 
     if (!isFound) {
         for (int i = 0; i < guesses.size(); ++i) {
-            if (tolower(letter) == tolower(guesses[i])) {
+            if (tolower(letterChar) == tolower(guesses[i])) {
                 cout << "You've already guessed this letter" << endl;
                 guessFound = true;
                 break;
@@ -48,9 +77,10 @@ bool wordToSolve::solve(char letter) {
 
         if (!guessFound) {
             attempts++;
-            guesses.push_back(toupper(letter));
+            guesses.push_back(toupper(letterChar));
         }
     }
+
 
     for (int i = 0; i < underscore.size(); i++) {
         if (underscore[i] == '_') {
@@ -61,6 +91,11 @@ bool wordToSolve::solve(char letter) {
 
     if (!underscoreFound) {return true;}
     else {return false;}
+}
+
+void wordToSolve::clearWordGuesses() {
+    wordGuesses.clear();
+
 }
 
 wordToSolve &wordToSolve::operator=(wordToSolve& otherWord) {
@@ -86,8 +121,13 @@ void wordToSolve::printGuesses() {
         cout << guesses[i] << " ";
     }
 
+    for(int g = 0; g < wordGuesses.size(); g++){
+        cout << wordGuesses[g] << " ";
+    }
+
     cout << endl;
 }
+
 
 bool wordToSolve::Solved() {
     for (int i = 0; i < underscore.size(); i++) {
